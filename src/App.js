58bd84bbd51576e65b152dcb631985bd61979d70
Peacock-Co <pavaoleonardo@hotmail.com>
2020-10-16 18:41:34 +0200

    first commit publishing gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Contact from './components/contact/Contact';
+import About from './components/about/About';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <Header />
+      <Switch>
+        <Route exact from='/' render={(props) => <Home {...props} />} />
+        <Route
+          exact
+          path='/contact'
+          render={(props) => <Contact {...props} />}
+        />
+        <Route exact path='/about' render={(props) => <About {...props} />} />
+      </Switch>
     </div>
   );
 }
