d4a1f2edc88c6ce99b23930e4e5aab9498c8b7a6
Peacock-Co <pavaoleonardo@hotmail.com>
2020-10-16 18:43:28 +0200

    Updates

---
+++ precache-manifest.de27a9f0a3a3ddd07fa5125322fbf68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c30c727ba83b6ec14e2c2ae8a353e43",
+    "url": "/aluguenahora/index.html"
+  },
+  {
+    "revision": "7264ee796ab33ac04266",
+    "url": "/aluguenahora/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb0a0cb84837998996cf",
+    "url": "/aluguenahora/static/js/2.d2ad0633.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/aluguenahora/static/js/2.d2ad0633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7264ee796ab33ac04266",
+    "url": "/aluguenahora/static/js/main.4fc31bf6.chunk.js"
+  },
+  {
+    "revision": "854d34716c7e9a37f7f0",
+    "url": "/aluguenahora/static/js/runtime-main.f9943323.js"
+  }
+]);
