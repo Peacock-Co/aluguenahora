b33e5d10d96ce9c9114479744ab8cc27132d0aba
Peacock-Co <pavaoleonardo@hotmail.com>
2020-10-20 11:03:15 +0200

    fixing routing on ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Contact from './components/contact/Contact';
@@ -9,17 +14,23 @@ const App = () => {
   return (
     <div>
       <Header />
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact from='/' render={(props) => <Home {...props} />} />
-          <Route
-            exact
-            path='/contact'
-            render={(props) => <Contact {...props} />}
-          />
-          <Route exact path='/about' render={(props) => <About {...props} />} />
-        </Switch>
-      </Router>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Router>
+          <Switch>
+            <Route exact from='/' render={(props) => <Home {...props} />} />
+            <Route
+              exact
+              path='/contact'
+              render={(props) => <Contact {...props} />}
+            />
+            <Route
+              exact
+              path='/about'
+              render={(props) => <About {...props} />}
+            />
+          </Switch>
+        </Router>
+      </BrowserRouter>
     </div>
   );
 };
