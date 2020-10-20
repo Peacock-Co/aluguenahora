66624ea3ee7a7a8fcfc0607841cf90664a52747e
Peacock-Co <pavaoleonardo@hotmail.com>
2020-10-20 11:46:06 +0200

    fixing routing on ghpages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       <Header />
       <Switch>
-        <Route exact path='#home' render={(props) => <Home {...props} />} />
+        <Route exact path='/' render={(props) => <Home {...props} />} />
         <Route
           exact
           path='/contact'
