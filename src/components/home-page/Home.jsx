5fabfdcc461511364139af5d5f1779fa69304531
Peacock-Co <pavaoleonardo@hotmail.com>
2020-11-09 16:33:31 +0100

    working on form and search bar

--- src/components/home-page/Home.jsx
+++ src/components/home-page/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 //Material UI
-import { Grid, Typography, Icon, Button, TextField } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  TextField,
+  FormControl,
+  Icon,
+} from '@material-ui/core';
+
 import { makeStyles } from '@material-ui/styles';
 
 //Lottie
@@ -21,6 +28,14 @@ const useStyles = makeStyles((theme) => ({
   searchContainer: {
     marginTop: '6em',
   },
+  title: {
+    textAlign: 'center',
+  },
+  bairroContainer: {
+    [theme.breakpoints.down('md')]: {
+      direction: 'column',
+    },
+  },
 }));
 
 function Home() {
@@ -38,46 +53,67 @@ function Home() {
   return (
     <Grid container direction='column' className={classes.mainContainer}>
       {/*Search block*/}
-      <Grid container direction='row'>
-        <Grid sm item container className={classes.animation}>
-          <Lottie
-            options={defaultOptions}
-            height={'100%'}
-            width={'100%'}
-            resizeMode='center'
-          />
-        </Grid>
+      <section>
+        <Grid container direction='row'>
+          <Grid sm={5} item container className={classes.animation}>
+            <Lottie
+              options={defaultOptions}
+              height={'100%'}
+              width={'100%'}
+              resizeMode='center'
+            />
+          </Grid>
 
-        <Grid sm item container className={classes.searchContainer}>
-          <Grid item container justify='center' direction='column'>
-            <Grid item>
+          <Grid
+            sm={7}
+            item
+            container
+            justify='center'
+            alignItems='center'
+            direction='column'
+            className={classes.searchContainer}
+          >
+            <Grid item className={classes.title}>
               <Typography variant='h2'>A blowfish! Cogitare. </Typography>
-              <Typography variant='h3'>Quid faciam blowfish, Isai. </Typography>
+              <Typography justify='center' variant='h3'>
+                Quid faciam blowfish, Isai.{' '}
+              </Typography>
             </Grid>
-            <Grid item container>
-              <Grid item>
-                <Icon />
-                <TextField placeholder='Busque por cidade' />
-              </Grid>
-            </Grid>
-            <Grid item container>
-              <Grid item>
-                <Icon />
-                <TextField placeholder='Busque por bairro' />
-              </Grid>
-              <Grid item>
-                <Icon />
-                <TextField placeholder='Busque por cidade' />
-              </Grid>
-              <Grid item>
-                <Icon />
-                <TextField placeholder='Busque por cidade' />
-              </Grid>
+            <Grid item container direction='column'>
+              <FormControl>
+                <Grid item>
+                  <Icon className='fas fa-map-marker-alt' />
+                  <label>
+                    <span>Cidade</span>
+                  </label>
+                  <TextField placeholder='Busque por cidade' fullWidth={true} />
+                </Grid>
+                <Grid container>
+                  <Grid item className={classes.bairroContainer}>
+                    <TextField xs='12' md='12' id='bairro' label='Bairro' />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      xs='12'
+                      md='3'
+                      id='aluguel'
+                      label='Aluguel até'
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      xs='6'
+                      md='3'
+                      id='quarto'
+                      label='Nº de quartos'
+                    />
+                  </Grid>
+                </Grid>
+              </FormControl>
             </Grid>
           </Grid>
-          <Button>Encontrar imóveis</Button>
         </Grid>
-      </Grid>
+      </section>
     </Grid>
   );
 }
