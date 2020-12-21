c1a6e07b9d98616a431408deebb8fa64329228ad
Peacock-Co <pavaoleonardo@hotmail.com>
2020-12-22 00:06:11 +0100

    Updates

---
+++ precache-manifest.88f3d9bab6cab24c800b7837f5fed9cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86fe673bbafb3941975883c70a74be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd435a23cf99acafc3fe",
+    "url": "/static/js/2.c2d54d01.chunk.js"
+  },
+  {
+    "revision": "d584d19d868e0c02863831be2b58edee",
+    "url": "/static/js/2.c2d54d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b361c14b10e1643f6c",
+    "url": "/static/js/main.4fbb09c2.chunk.js"
+  },
+  {
+    "revision": "93ae8ca29a34c4cc5e60",
+    "url": "/static/js/runtime-main.a50da8c4.js"
+  },
+  {
+    "revision": "f496b5f004afa9a5d8eb32909a0cd494",
+    "url": "/static/media/aluguenahora.logo.f496b5f0.svg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/static/media/facebook-square-brands.34e73505.svg"
+  },
+  {
+    "revision": "6c6931f426bcc5ba571c8ed56b757ece",
+    "url": "/static/media/instagram-square-brands.6c6931f4.svg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/static/media/linkedin-brands.d9049f46.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/static/media/twitter-brands.6620c8ae.svg"
+  }
+]);
