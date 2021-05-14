5f2643d2b1718f16202090bc48e8fb12615f2da9
Peacock-Co <pavaoleonardo@hotmail.com>
2021-05-14 18:31:19 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aluguenahora/precache-manifest.e18e106716f59e0a459207b6acc77440.js"
+  "/aluguenahora/precache-manifest.487d06b5435805e74e270371c3672ba8.js"
 );
 
 self.addEventListener('message', (event) => {
